2149dbc00f97c725feace90c693a476635901163
mib <marc.brewer@balanced-technology.de>
2025-09-24 09:38:29 +0200

    Initial COmmit

---
+++ src/main/java/de/brewer/microservice_base/ConfigServerBaseApplication.java
@@ -0,0 +1,13 @@
+package de.brewer.microservice_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/brewer/microservice_base/ConfigServerBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.brewer.microservice_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
